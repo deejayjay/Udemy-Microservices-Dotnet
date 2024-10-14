722674bd1c911ed73f036b75392b68b869039fcf
DeeJayJay <deejayjay@zoho.com>
2024-10-14 14:38:00 -0600

    Adds ShoppingCart API project with all initial Models and Dtos.

---
+++ Mango/Mango.Services.ShoppingCartAPI/Models/Dtos/CartHeaderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dtos;
+
+public class CartHeaderDto
+{
+    public int CartHeaderId { get; set; }
+    public string UserId { get; set; }
+    public string? CouponCode { get; set; }
+    public decimal Discount { get; set; }
+    public decimal CartTotal { get; set; }
+}
